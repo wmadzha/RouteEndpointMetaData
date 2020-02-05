7fdd3c099bb4b0aaf0c961e4c9d6402d1fec971e
Rick Anderson <riande@microsoft.com>
2020-02-04 14:06:58 -1000

    init

---
+++ WebRouting/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+
+namespace WebRouting.Controllers
+{
+    [Route("Home2")]
+    public class DefaultController : Controller
+    {
+        [Route("Index")]
+        [Route("/xyz")]
+        public IActionResult Index()
+        {
+            var template = ControllerContext.ActionDescriptor.AttributeRouteInfo.Template;
+            var actionName = ControllerContext.ActionDescriptor.ActionName;
+            var controllerName = ControllerContext.ActionDescriptor.ControllerName;
+
+            return Content($"template:{template} " +
+                $" controller:{controllerName}  action name: {actionName}");
+        }
+    }
+}
---
+++ WebRouting/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebRoute.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var template = ControllerContext.ActionDescriptor.AttributeRouteInfo.Template;
+            var actionName = ControllerContext.ActionDescriptor.ActionName;
+            var controllerName = ControllerContext.ActionDescriptor.ControllerName;
+
+            return Content($"template:{template} " +
+                $" controller:{controllerName}  action name: {actionName}");
+        }
+
+        public IActionResult Privacy()
+        {
+            var template = ControllerContext.ActionDescriptor.AttributeRouteInfo.Template;
+            var actionName = ControllerContext.ActionDescriptor.ActionName;
+            var controllerName = ControllerContext.ActionDescriptor.ControllerName;
+
+            return Content($"template:{template} " +
+                $" controller:{controllerName}  action name: {actionName}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            var template = ControllerContext.ActionDescriptor.AttributeRouteInfo.Template;
+            var actionName = ControllerContext.ActionDescriptor.ActionName;
+            var controllerName = ControllerContext.ActionDescriptor.ControllerName;
+
+            return Content($"template:{template} " +
+                $" controller:{controllerName}  action name: {actionName}");
+        }
+    }
+}
